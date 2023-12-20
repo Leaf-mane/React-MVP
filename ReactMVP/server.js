d32ebadbe0c716310ae8d9978303a41315f705de
NOAH MORRISON <morrison.noah@gmail.com>
2023-12-20 07:42:41 -0800

    reformat server.js2. One day I will figure this out.

--- ReactMVP/server.js
+++ ReactMVP/server.js
@@ -1,12 +1,16 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import express from 'express';
-import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
